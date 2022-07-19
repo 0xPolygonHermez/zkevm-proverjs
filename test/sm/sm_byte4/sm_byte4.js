ca3dacc40ad0ffd00af991251e2757766997d256
zkronos73 <zkronos73@gmail.com>
2022-07-19 13:13:59 +0200

    test at one plookup col

---
+++ test/sm/sm_byte4/sm_byte4.js
@@ -0,0 +1,43 @@
+const chai = require("chai");
+const assert = chai.assert;
+const fs = require("fs");
+const F1Field = require("ffjavascript").F1Field;
+
+const { newConstantPolsArray, newCommitPolsArray, compile, verifyPil } = require("pilcom");
+const smPoseidonG = require("../src/sm/sm_byte4.js");
+
+
+describe("test byte4 state machine", async function () {
+    this.timeout(10000000);
+
+    it("It should check standard l", async () => {
+        const F = new F1Field("0xFFFFFFFF00000001");
+
+        const pil = await compile(F, "main_byte4.pil");
+        const constPols = newConstantPolsArray(pil);
+        const cmPols = newCommitPolsArray(pil);
+
+        const n1 = F.e(-1);
+        const input = [
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,  0,  0, 0x3c18a9786cb0b359n, 0xc4055e3364a246c3n, 0x7953db0ab48808f4n, 0xc71603f33a1144can],
+            [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 0xd64e1e3efc5b8e9en, 0x53666633020aaa47n, 0xd40285597c6a8825n, 0x613a4f81e81231d2n],
+            [n1, n1, n1, n1, n1, n1, n1, n1, n1, n1, n1, n1, 0xbe0085cfc57a8357n, 0xd95af71847d05c09n, 0xcf55a13d33c1c953n, 0x95803a74f4530e82n]
+        ];
+
+
+        await smByte4.buildConstants(constPols);
+        await smByte4.execute(cmPols, input);
+
+        // Verify
+        const res = await verifyPil(F, pil, cmPols, constPols);
+
+        if (res.length != 0) {
+            console.log("Pil does not pass");
+            for (let i = 0; i < res.length; i++) {
+                console.log(res[i]);
+            }
+            assert(0);
+        }
+    });
+
+});
