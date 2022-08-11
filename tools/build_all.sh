daa6e8121e51b8de125380a2bb33199eca985c8b
Jordi Baylina <jordi@baylina.cat>
2022-08-11 22:30:23 +0000

    fix error on build script and automate g16setup random contribution

--- tools/build_all.sh
+++ tools/build_all.sh
@@ -4,14 +4,13 @@ echo "####### START $(date +'%Y-%m-%d %H:%M:%S') ########" >> $BDIR/steps.log
 LAST_STEP_FILE=$BDIR/last_step.txt
 [ ! -z $npm_config_from ] && SKIP=1
 [ $npm_config_continue ] && [ -f $LAST_STEP_FILE ] && LAST_STEP=`cat $LAST_STEP_FILE` && echo "last step done: $LAST_STEP" && SKIP=1
-PREV_STEP="**"
 START_TIME=$(date +%s)
 while [ $# -gt 0 ]; do
     PREV_STEP=$STEP
     STEP=$1
     shift
     [ "$npm_config_from" = "$STEP" ] && SKIP=0
-    [ "$PREV_STEP" = "$LAST_STEP" ] && SKIP=0
+    [ ! -z "$PREV_STEP" ] && [ "$PREV_STEP" = "$LAST_STEP" ] && SKIP=0
     [ $SKIP -eq 1 ] && continue
     echo "\e[35;1m####### $STEP #######\e[0m"
     START_STEP_TIME=$(date +%s)
