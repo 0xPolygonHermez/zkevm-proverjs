ce850478d38132922f78f7147a6449e3ffcb4ba8
zkronos73 <zkronos73@gmail.com>
2022-10-02 11:32:20 +0200

    improve storage test and  tests tools

--- test/counters/storage.js
+++ test/counters/storage.js
@@ -10,7 +10,16 @@ describe("Test Storage Counters", async function () {
 
     it("Verify Storage Zkasm Test", async () => {
         await verifyZkasm("../zkasm/counters/storage.zkasm", true,
-            { defines: {N: 2 ** 21},
+            { defines: {N: 2 ** 16},
+              namespaces: ['Global', 'Main', 'Storage'],
+              verbose: true,
+              color: true,
+              disableUnusedError: true});
+    });
+
+    it("Verify Storage Zkasm Test (some delete edge cases)", async () => {
+        await verifyZkasm("../zkasm/counters/storage2.zkasm", true,
+            { defines: {N: 2 ** 16},
               namespaces: ['Global', 'Main', 'Rom', 'Byte4', 'Storage', 'PoseidonG'],
               verbose: true,
               color: true,
