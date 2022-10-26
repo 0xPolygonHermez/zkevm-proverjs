6ac43a68f44a941958c801d888b115d036ed72cc
Jordi Baylina <jordi@baylina.cat>
2022-10-26 09:57:39 +0000

    package progress

--- pre.sh
+++ pre.sh
@@ -1,3 +1,13 @@
+checkMandatoryOptArg() {
+    argname=$3
+    [ "$2" = "true" ] && echo "ERROR: --$1 without mandatory argument. usage: --$1=<${argname:=value}>" && exit 1
+}
+
+checkMandatoryOptArg build $npm_config_build buildpath
+checkMandatoryOptArg pil $npm_config_pil file.pil
+checkMandatoryOptArg pilconfig $npm_config_pilconfig pilconfig.json
+checkMandatoryOptArg nth $npm_config_nth
+
 BDIR="${npm_config_build:=build/proof}"
 mkdir -p $BDIR
 # NODE="--trace-gc --trace-gc-ignore-scavenger --max-semi-space-size=1024 --max-old-space-size=524288"
@@ -11,4 +21,6 @@ PILSTARK="node $NODE node_modules/pil-stark/src"
 PILCOM="node $NODE node_modules/pilcom/src"
 SNARKJS="node $NODE node_modules/snarkjs/cli.js"
 BCTREE="${npm_config_bctree:=$PILSTARK/main_buildconsttree.js}"
+# [ ! -z $npm_config_nth ] &&
+NTH="${npm_config_nth}"
 true
