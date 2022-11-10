88da65acb5a6accd0a3510f61e9630189ca7a051
zkronos73 <zkronos73@gmail.com>
2022-11-10 04:00:56 +0000

    fix join zkin

--- src/main_joinzkin.js
+++ src/main_joinzkin.js
@@ -2,6 +2,7 @@ const fs = require("fs");
 const path = require("path");
 const { polMulAxi } = require("pil-stark/src/polutils");
 const version = require("../package").version;
+const JSONbig = require("json-bigint");
 
 const argv = require("yargs")
     .version(version)
@@ -10,19 +11,21 @@ const argv = require("yargs")
 
 async function run() {
 
-    const zkin11File = typeof(argv.zkin2) === "string" ?  argv.zkin2.trim() : "zkin2.json";
+    const zkin1File = typeof(argv.zkin1) === "string" ?  argv.zkin1.trim() : "zkin1.json";
     const zkin2File = typeof(argv.zkin2) === "string" ?  argv.zkin2.trim() : "zkin2.json";
-    const zkinOutFile = typeof(argv.zkinOut) === "string" ?  argv.zkinOut : "zkinOut.json";
+    const zkinOutFile = typeof(argv.zkinout) === "string" ?  argv.zkinout : "zkinOut.json";
     const verKeyFile = typeof(argv.verkey) === "string" ?  argv.verkey.trim() : "recursive2.verkey.json";
 
 
-    const zkin1 = JSON.parse(await fs.promises.readFile(zkin11File, "utf8"));
+    const zkin1 = JSON.parse(await fs.promises.readFile(zkin1File, "utf8"));
     const zkin2 = JSON.parse(await fs.promises.readFile(zkin2File, "utf8"));
     const verKey = JSONbig.parse(await fs.promises.readFile(verKeyFile, "utf8"));
     const constRoot = verKey.constRoot;
 
 
     const zkinOut = {};
+
+    zkinOut.publics = [];
 
     for (let i=0; i<8; i++) zkinOut.publics[0+i] = zkin1.publics[0+i];  // oldStateRoot
 
@@ -48,11 +51,14 @@ async function run() {
     for (let i=0; i<8; i++) zkinOut.publics[26+i] = zkin2.publics[26+i];  // newAccInputHash0
     for (let i=0; i<8; i++) zkinOut.publics[34+i] = zkin2.publics[34+i];  // newLocalExitRoot
 
+    zkinOut.publics[42] = zkin2.publics[42];  // oldBatchNum
+
     zkinOut.a_publics = zkin1.publics;
     zkinOut.a_root1 = zkin1.root1;
     zkinOut.a_root2 = zkin1.root2;
     zkinOut.a_root3 = zkin1.root3;
     zkinOut.a_root4 = zkin1.root4;
+    zkinOut.a_evals = zkin1.evals;
     zkinOut.a_s0_vals1 = zkin1.s0_vals1;
     zkinOut.a_s0_vals3 = zkin1.s0_vals3;
     zkinOut.a_s0_vals4 = zkin1.s0_vals4;
@@ -68,14 +74,19 @@ async function run() {
     zkinOut.a_s1_siblings = zkin1.s1_siblings;
     zkinOut.a_s2_siblings = zkin1.s2_siblings;
     zkinOut.a_s3_siblings = zkin1.s3_siblings;
+    zkinOut.a_s4_siblings = zkin1.s4_siblings;
+    zkinOut.a_s1_vals = zkin1.s1_vals;
+    zkinOut.a_s2_vals = zkin1.s2_vals;
+    zkinOut.a_s3_vals = zkin1.s3_vals;
+    zkinOut.a_s4_vals = zkin1.s4_vals;
     zkinOut.a_finalPol = zkin1.finalPol;
-
 
     zkinOut.b_publics = zkin2.publics;
     zkinOut.b_root1 = zkin2.root1;
     zkinOut.b_root2 = zkin2.root2;
     zkinOut.b_root3 = zkin2.root3;
     zkinOut.b_root4 = zkin2.root4;
+    zkinOut.b_evals = zkin2.evals;
     zkinOut.b_s0_vals1 = zkin2.s0_vals1;
     zkinOut.b_s0_vals3 = zkin2.s0_vals3;
     zkinOut.b_s0_vals4 = zkin2.s0_vals4;
@@ -91,9 +102,17 @@ async function run() {
     zkinOut.b_s1_siblings = zkin2.s1_siblings;
     zkinOut.b_s2_siblings = zkin2.s2_siblings;
     zkinOut.b_s3_siblings = zkin2.s3_siblings;
+    zkinOut.b_s4_siblings = zkin2.s4_siblings;
+    zkinOut.b_s1_vals = zkin2.s1_vals;
+    zkinOut.b_s2_vals = zkin2.s2_vals;
+    zkinOut.b_s3_vals = zkin2.s3_vals;
+    zkinOut.b_s4_vals = zkin2.s4_vals;
     zkinOut.b_finalPol = zkin2.finalPol;
 
-    zkinOut.rootC = constRoot;
+    zkinOut.rootC = [];
+    for (let i=0; i<4; i++) {
+        zkinOut.rootC[i] = constRoot[i].toString();
+    }
 
     await fs.promises.writeFile(zkinOutFile, JSON.stringify(zkinOut, null, 1), "utf8");
 
