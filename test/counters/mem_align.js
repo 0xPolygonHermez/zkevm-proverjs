3e651d8bd2d73ce3a12c09e500c5e63a599266ea
zkronos73 <zkronos73@gmail.com>
2022-11-11 15:21:45 +0100

    change cycle of constants and finish pil verification.

--- test/counters/mem_align.js
+++ test/counters/mem_align.js
@@ -9,8 +9,8 @@ describe("Test MemAlign Counter", async function () {
     this.timeout(10000000000);
 
     it("Verify MemAlign Zkasm Test", async () => {
-        await verifyZkasm("../zkasm/counters/mem_align.zkasm", true,
-                { defines: {N: 2 ** 19},
+        await verifyZkasm("../zkasm/counters/mem_align.zkasm", {continueOnError: true},
+                { defines: {N: 2 ** 21},
                   namespaces: ['Global', 'Main', 'Rom', 'Byte4', 'MemAlign'],
                   verbose: true,
                   color: true,
