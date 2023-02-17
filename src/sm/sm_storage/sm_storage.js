fd7104b2a2839c73e2a27c260a174fe692990393
zkronos73 <zkronos73@gmail.com>
2023-02-17 08:17:28 +0000

    move storage-rom

--- src/sm/sm_storage/sm_storage.js
+++ src/sm/sm_storage/sm_storage.js
@@ -13,7 +13,7 @@ module.exports.buildConstants = async function (pols) {
     const fr = poseidon.F;
 
     // Init rom from file
-    const rawdata = fs.readFileSync("testvectors/storage_sm_rom.json");
+    const rawdata = fs.readFileSync(__dirname + "/storage_sm_rom.json");
     const j = JSON.parse(rawdata);
     rom = new StorageRom;
     rom.load(j);
