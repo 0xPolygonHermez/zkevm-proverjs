7e6185f7af1dd6681e429fbc3de9c8dc2e2394e0
eduadiez <eduadiez@gmail.com>
2023-02-20 21:44:41 +0100

    adaptation to fflonk

--- tools/copy_files.sh
+++ tools/copy_files.sh
@@ -172,9 +172,9 @@ if [ $CP_FINAL -eq 1 ]; then
 # final
 FULLDST=$DST/config/final
 [ ! -d $FULLDST ] && mkdir -p           $FULLDST
-cpfile $BDIR/final.g16.0001.zkey           $FULLDST
+cpfile $BDIR/final.fflonk.zkey          $FULLDST
 cpfile $BDIR/final_cpp/final.dat           $FULLDST/final.verifier.dat
-cpfile $BDIR/final.g16.verkey.json         $FULLDST
+cpfile $BDIR/final.fflonk.verkey.json         $FULLDST
 cpdir $BDIR/final_cpp                  $DST/c_files
 fi
 
@@ -198,7 +198,7 @@ NPMFILE=$BDIR/npm.txt
 [ ! -f $NPMFILE ] && npm ls > $NPMFILE
 cpfile $NPMFILE $FULLDST
 
-BUILDS="sha256.txt steps.log c12a.starkstruct.json final.g16.0000.zkey final.g16.verifier.sol final.r1cs final.sym recursive.starkstruct.json recursive1.r1cs recursive1.sym recursive2.r1cs recursive2.sym recursivef.r1cs recursivef.starkstruct.json recursivef.sym zkevm.starkstruct.json zkevm.verifier.r1cs zkevm.verifier.sym"
+BUILDS="sha256.txt steps.log c12a.starkstruct.json final.fflonk.zkey final.fflonk.verifier.sol final.r1cs final.sym recursive.starkstruct.json recursive1.r1cs recursive1.sym recursive2.r1cs recursive2.sym recursivef.r1cs recursivef.starkstruct.json recursivef.sym zkevm.starkstruct.json zkevm.verifier.r1cs zkevm.verifier.sym"
 for F in $BUILDS; do
     cpfile $BDIR/$F $FULLDST
 done
