ad08996211b2c4a13be0fad334af51775b960826
Jordi Baylina <jordi@baylina.cat>
2023-11-06 16:50:18 +0100

    All sm working except sha256f

---
+++ test/sm/sm_padding_sha256_test.js
@@ -0,0 +1,104 @@
+const chai = require('chai');
+const { createHash } = require('node:crypto');
+
+const { assert } = chai;
+const { F1Field } = require('ffjavascript');
+
+const {
+    newConstantPolsArray, newCommitPolsArray, compile, verifyPil,
+} = require('pilcom');
+
+const smPaddingSha256 = require('../../src/sm/sm_padding_sha256');
+const smPaddingSha256Bit = require('../../src/sm/sm_padding_sha256bit/sm_padding_sha256bit');
+const smBits2FieldSha256 = require('../../src/sm/sm_bits2field_sha256');
+// const smSha256F = require('../../src/sm/sm_sha256/sm_sha256');
+const smGlobal = require('../../src/sm/sm_global');
+const { sha256 } = require('../../src/sm/sm_padding_sha256bit/sha256');
+
+// input = [];
+
+const input = [
+    {
+        data: '0102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f20',
+        reads: [32],
+    },
+    {
+        data:
+            '0102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f20'
+            + '2122232425262728292a2b2c2d2e2f303132333435363738393a3b3c3d3e3f40'
+            + '0102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f20'
+            + '2122232425262728292a2b2c2d2e2f303132333435363738393a3b3c3d3e3f40'
+            + '0102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f20'
+            + '2122232425262728292a2b2c2d2e2f303132333435363738393a3b3c3d3e3f40',
+        reads: [1, 2, 3, 4, 5, 6, 7, 5, 31],
+    },
+];
+
+describe('test padding sha256', async function () {
+    this.timeout(10000000);
+
+    it('should sha256 the abc', async () => {
+        const hash0 = sha256('abc', 'hex');
+        const hash0expected = createHash('sha256').update('abc').digest('hex');
+        assert.equal(hash0, hash0expected);
+    });
+
+    it('should sha256 the empty', async () => {
+        const hash0 = sha256('', 'hex');
+        const hash0expected = createHash('sha256').update(Uint8Array.from([])).digest('hex');
+        assert.equal(hash0, hash0expected);
+    });
+
+    it('should sha256 the long string', async () => {
+        let s = '';
+        while (s.length <= 1025) {
+            const hash0 = sha256(s, 'hex');
+            const hash0expected = createHash('sha256').update(s).digest('hex');
+            assert.equal(hash0, hash0expected);
+            s += 'a';
+        }
+    });
+
+    it('It should create the pols sha256 padding', async () => {
+        const Fr = new F1Field('0xFFFFFFFF00000001');
+        const pil = await compile(Fr, 'pil/padding_sha256.pil', null, { defines: { N: 2 ** 20 } });
+        const constPols = newConstantPolsArray(pil);
+        const cmPols = newCommitPolsArray(pil);
+        await smPaddingSha256.buildConstants(constPols.PaddingSha256);
+        await smPaddingSha256Bit.buildConstants(constPols.PaddingSha256Bit);
+        await smBits2FieldSha256.buildConstants(constPols.Bits2FieldSha256);
+        // await smSha256F.buildConstants(constPols.Sha256F);
+        await smGlobal.buildConstants(constPols.Global);
+
+        for (let i = 0; i < constPols.$$array.length; i++) {
+            const arr = constPols.$$array[i];
+            for (let j = 0; j < arr.length; j++) {
+                assert(typeof (arr[j]) === 'bigint',`const pol: ${i} w: ${j} desc: ${constPols.$$defArray[i]}`);
+            }
+        }
+
+        const requiredSha256 = await smPaddingSha256.execute(cmPols.PaddingSha256, input);
+        const requiredSha256bit = await smPaddingSha256Bit.execute(cmPols.PaddingSha256Bit, requiredSha256.paddingSha256Bit);
+        const requiredBits2FieldSha256 = await smBits2FieldSha256.execute(cmPols.Bits2FieldSha256, requiredSha256bit.Bits2FieldSha256);
+        // await smSha256F.execute(cmPols.KeccakF, requiredBits2FieldSha256.KeccakF);
+
+        for (let i = 0; i < cmPols.$$array.length; i++) {
+            const arr = cmPols.$$array[i];
+            for (let j = 0; j < arr.length; j++) {
+                assert(typeof (arr[j]) === 'bigint', `pol: ${i} w: ${j} desc: ${cmPols.$$defArray[i]}`);
+            }
+        }
+
+        const res = await verifyPil(Fr, pil, cmPols, constPols);
+
+        if (res.length !== 0) {
+            // eslint-disable-next-line no-console
+            console.log('Pil does not pass');
+            for (let i = 0; i < res.length; i++) {
+                // eslint-disable-next-line no-console
+                console.log(res[i]);
+            }
+            assert(0);
+        }
+    });
+});
---
+++ tools/sha256/gen_sha256.js
@@ -0,0 +1,321 @@
+const BitsPerField = 6;
+
+function generateK(ctx) {
+    const K = [
+        0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
+        0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
+        0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
+        0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
+        0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
+        0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
+        0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
+        0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2,
+    ];
+
+    const res = [];
+    for (let i = 0; i < K.length; i++) {
+        const v32 = [];
+        for (let j = 0; j < 32; j++) {
+            if ((K[i] >>> (31 - j)) & 1) {
+                v32.push(ctx.one);
+            } else {
+                v32.push(ctx.zero);
+            }
+        }
+        res.push(v32);
+    }
+
+    return res;
+}
+
+function newWire(ctx) {
+    ctx.wiresUsed += 1;
+
+    return ctx.wiresUsed-1;
+}
+
+function isNextAvailable(ctx) {
+    if (ctx.gatesUsed % BitsPerField !== (BitsPerField -1)) return true;
+    const b = Math.floor(ctx.gatesUsed / BitsPerField);
+    if (b < 1024) return true;
+
+    return false;
+}
+
+function getNextAvailableGate(ctx) {
+    while (!isNextAvailable(ctx)) {
+        ctx.gates[ctx.gatesUsed] = {
+            type: null,
+            connections: [null, null, null, null],
+        };
+        ctx.gatesUsed += 1;
+    }
+    const res = ctx.gatesUsed;
+    ctx.gates[ctx.gatesUsed] = {
+        type: null,
+        connections: [null, null, null, null],
+    };
+    ctx.gatesUsed += 1;
+
+    return res;
+}
+
+function add32(ctx, a, b) {
+    const out = [];
+    let lprev;
+    {
+        const l = getNextAvailableGate(ctx);
+        ctx.program.push({
+            in1: a,
+            in2: b,
+            in3: ctx.zero,
+            op: 'add',
+            ref: l,
+        });
+        ctx.gates[l].type = 'add';
+        ctx.gates[l].connections[0] = a.wire;
+        ctx.gates[l].connections[1] = b.wire;
+        ctx.gates[l].connections[2] = ctx.zero.wire;
+        ctx.gates[l].connections[3] = newWire(ctx);
+        out[31] = {
+            type: 'wired',
+            gate: l,
+            pin: 'out',
+            wire: ctx.gates[l].connections[3],
+        };
+        lprev = l;
+    }
+    for (let i = 32; i >= 1; i--) {
+        const l = getNextAvailableGate(ctx);
+        if (l === lprev + 1) {
+            ctx.program.push({
+                in1: a,
+                in2: b,
+                op: 'add',
+                ref: l,
+            });
+            ctx.gates[l].type = 'add';
+            ctx.gates[l].connections[0] = a.wire;
+            ctx.gates[l].connections[1] = b.wire;
+            ctx.gates[l].connections[2] = newWire(ctx);
+            ctx.gates[l].connections[3] = newWire(ctx);
+            out[i] = {
+                type: 'wired',
+                gate: l,
+                pin: 'out',
+                wire: ctx.gates[l].connections[3],
+            };
+        } else {
+            // lprev + 1 is ether an input or a output
+            const carryWire = newWire(ctx);
+            ctx.gates[lprev + 1].connections[2] = carryWire;
+            ctx.program.push({
+                in1: a,
+                in2: b,
+                in3: {
+                    type: 'wired',
+                    gate: lprev + 1,
+                    pin: 'in3',
+                    wire: carryWire,
+                },
+                op: (i === 0) ? 'xor' : 'add',
+                ref: l,
+            });
+            ctx.gates[l].type = (i === 0) ? 'xor' : 'add';
+            ctx.gates[l].connections[0] = a.wire;
+            ctx.gates[l].connections[1] = b.wire;
+            ctx.gates[l].connections[2] = carryWire;
+            ctx.gates[l].connections[3] = newWire(ctx);
+            out[i] = {
+                type: 'wired',
+                gate: l,
+                pin: 'out',
+                wire: ctx.gates[l].connections[3],
+            };
+        }
+        lprev = l;
+    }
+
+    return out;
+}
+
+function gate(ctx, gate, a, b, c) {
+    const l = getNextAvailableGate(ctx);
+    ctx.program.push({
+        in1: a,
+        in2: b,
+        in3: c,
+        op: gate,
+        ref: l,
+    });
+    ctx.gates[l].type = gate;
+    ctx.gates[l].connections[0] = a.wire;
+    ctx.gates[l].connections[1] = b.wire;
+    ctx.gates[l].connections[2] = c.wire;
+    ctx.gates[l].connections[3] = newWire(ctx);
+    const out = {
+        type: 'wired',
+        gate: l,
+        pin: 'out',
+        wire: ctx.gates[l].connections[3],
+    };
+
+    return out;
+}
+
+function gate32(ctx, gate, op1, op2, op3) {
+    const res = [];
+    for (let i = 0; i < 32; i++) {
+        res.push(gate(ctx, gate, op1[i], op2[i], op3[i]));
+    }
+
+    return res;
+}
+
+function xor32(ctx, op1, op2, op3) {
+    return gate32(ctx, 'xor', op1, op2, op3);
+}
+
+function ch(ctx, op1, op2, op3) {
+    return gate32(ctx, 'ch', op1, op2, op3);
+}
+
+function maj(ctx, op1, op2, op3) {
+    return gate32(ctx, 'maj', op1, op2, op3);
+}
+
+function rotr(ctx, a, n) {
+    const out = [];
+    for (let i = 0; i < 32; i++) {
+        out[(i + n) % 32] = a[i];
+    }
+
+    return out;
+}
+
+function shr(ctx, a, n) {
+    const out = [];
+    for (let i = 0; i < n; i++) {
+        out[i] = ctx.zero;
+    }
+    for (let i = n; i < 32; i++) {
+        out[i] = a[i - n];
+    }
+
+    return out;
+}
+
+function sigmaLow0(ctx, s32) {
+    const op1 = rotr(ctx, s32, 7);
+    const op2 = rotr(ctx, s32, 18);
+    const op3 = shr(ctx, s32, 3);
+
+    return xor32(ctx, op1, op2, op3);
+}
+
+function generateCircuit() {
+    const ctx = {};
+
+    ctx.zero = {
+        type: 'wired',
+        gate: 0,
+        pin: 'in1',
+        wire: 0,
+    };
+
+    ctx.one = {
+        type: 'wired',
+        gate: 0,
+        pin: 'in2',
+        wirw: 1,
+    };
+    ctx.gatesUsed = 0;
+    ctx.wiresUsed = 2;
+    ctx.program = [];
+    ctx.connections = [];
+
+    const stIn = [];
+    for (let i = 0; i < 8; i++) {
+        stIn[i] = [];
+        for (let j = 0; j < 32; j++) {
+            stIn[i].push({
+                type: 'inputState',
+                bit: i * 32 + j,
+                wire: newWire(ctx),
+            });
+        }
+    }
+
+    let [a, b, c, d, e, f, g, h] = stIn;
+
+    const w = [];
+    for (let i = 0; i < 16; i++) {
+        w[i] = [];
+        for (let j = 0; j < 32; j++) {
+            w[i].push({
+                type: 'input',
+                bit: i * 32 + j,
+                wire: newWire(ctx),
+            });
+        }
+    }
+
+    let K = generateK(ctx);
+
+    for (let t = 16; t < 64; t++) {
+        w[t] = sigmaLow1(ctx, w[i - 2]);
+        w[t] = add32(ctx, w[t], w[t - 7]);
+        w[t] = add32(ctx, w[t], sigmaLow0(ctx, w[t - 15]));
+        w[t] = add32(ctx, w[t], w[t - 16]);
+    }
+
+    for (let t = 0; t < 64; t++) {
+        let t1 = h;
+        t1 = add32(ctx, t1, sigmaBig1(e));
+        t1 = add32(ctx, t1, ch(e, f, g));
+        t1 = add32(ctx, t1, K[t]);
+        t1 = add32(ctx, t1, W[t]);
+
+        let t2 = sigmaBig0(a);
+        t1 = add32(ctx, t2, maj(a, b, c));
+
+        h = g;
+        g = f;
+        f = e;
+        e = add32(ctx, d, t1);
+        d = c;
+        c = b;
+        b = a;
+        a = add32(ctx, t1, t2);
+    }
+
+    const sOut = [];
+    sOut[0] = add32(ctx, stIn[0], a);
+    sOut[1] = add32(ctx, stIn[0], b);
+    sOut[2] = add32(ctx, stIn[0], c);
+    sOut[3] = add32(ctx, stIn[0], d);
+    sOut[4] = add32(ctx, stIn[0], e);
+    sOut[5] = add32(ctx, stIn[0], f);
+    sOut[6] = add32(ctx, stIn[0], g);
+    sOut[7] = add32(ctx, stIn[0], g);
+
+    for (let i = 0; i < 8; i++) {
+        for (let j = 0; j < 32; j++) {
+            const p = (i * 32 + j) * BitsPerField + (BitsPerField - 1);
+            ctx.gates[p].connections[0] = stIn[i][j].wire;
+            ctx.gates[p].connections[1] = ctx.zero;
+            ctx.gates[p].connections[3] = newWire(ctx);
+            const inst = {
+                in1: stIn[i][j],
+                in2: ctx.zero,
+                op: 'xor',
+                ref: p,
+            };
+            if (ctx.gates[p].connections[2] === null) {
+                inst.in3 = ctx.zero;
+                ctx.gates[p].connections[2] = ctx.zero.wire;
+            }
+            ctx.program.push(inst);
+        }
+    }
+}
