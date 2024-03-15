8ec859df4e142217ce8e25cb1c96a211082e2d52
RogerTaule <roger.taulee@gmail.com>
2024-03-15 11:45:43 +0100

    Cleaning code

--- src/main_gencircom.js
+++ src/main_gencircom.js
@@ -19,6 +19,65 @@ const argv = require("yargs")
     .string("arity")
     .argv;
 
+const batchPublicsEip4844 = {
+    oldStateRootPos: 0,
+    oldBatchAccInputHashPos: 8,
+    previousL1InfoTreeRootPos: 16,
+    previousL1InfoTreeIndexPos: 24,
+    chainIdPos: 25,
+    forkIdPos: 26,
+    newStateRootPos: 27,
+    newBatchAccInputHashPos: 35,
+    currentL1InfoTreeRootPos: 43,
+    currentL1InfoTreeIndexPos: 51,
+    newLocalExitRootPos: 52,
+    newLastTimestampPos: 59,
+};
+
+const batchPublics = {
+    oldStateRootPos: 0,
+    oldBatchAccInputHashPos: 8,
+    oldBatchNumPos: 16,
+    chainIdPos: 17,
+    forkIdPos: 18,
+    newStateRootPos: 19,
+    newBatchAccInputHashPos: 27,
+    newLocalExitRootPos: 35,
+    newBatchNumPos: 43,
+};
+
+const blobInnerPublics = {
+    oldBlobStateRootPos: 0,
+    oldBlobAccInputHashPos: 8,
+    oldBlobNumPos: 16,
+    oldStateRootPos: 17,
+    forkIdPos: 25,
+    newBlobStateRootPos: 26,
+    newBlobAccInputHashPos: 34,
+    newBlobNumPos: 42,
+    finalAccBatchHashDataPos: 43,
+    localExitRootFromBlobPos: 51,
+    isInvalidPos: 59,
+    timestampLimitPos: 60,
+    lastL1InfoTreeRootPos: 61,
+    lastL1InfoTreeIndexPos: 69,
+};
+
+const blobOuterPublics = {
+    oldStateRootPos: 0,
+    oldBlobStateRootPos: 8,
+    oldBlobAccInputHashPos: 16,
+    oldBlobNumPos: 24,
+    chainIdPos: 25,
+    forkIdPos: 26,
+    newStateRootPos: 27,
+    newBlobStateRootPos: 35,
+    newBlobAccInputHashPos: 43,
+    newBlobNumPos: 51,
+    newLocalExitRootPos: 52,
+};
+
+
 async function run() {
     const template = argv.template;
     if(!template) throw new Error("A template name must be provided!");
@@ -89,7 +148,19 @@ async function run() {
 
     const circomTemplate = await fs.promises.readFile(path.join(__dirname, "templates", `${template}.circom.ejs`), "utf8");
 
-    const circomVerifier = ejs.render(circomTemplate, {nStages, starkInfoVerifiers, vks, isBatchRecursion, verifierNames, isEip4844, arity});
+    const optionsCircom = {
+        nStages,
+        starkInfoVerifiers,
+        vks,
+        isBatchRecursion,
+        verifierNames,
+        isEip4844,
+        arity,
+        batchPublics: isEip4844 ? batchPublicsEip4844 : batchPublics,
+        blobInnerPublics,
+        blobOuterPublics,
+    };
+    const circomVerifier = ejs.render(circomTemplate, optionsCircom);
     await fs.promises.writeFile(recursiveFile, circomVerifier, "utf8");
 
     console.log("file Generated Correctly");
